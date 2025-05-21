031824b6f98481ee2db77726290137c9940492a7
Sleonow <kazanofffa19846494@gmail.com>
2025-05-21 18:30:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_TOKEN = os.getenv("API_TOKEN")
+API_TOKEN = 8042965148:AAHjoSaOopUau4CiaDEcov0uimtvGwm8Dlc
 WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 
 bot = telebot.TeleBot(API_TOKEN)
