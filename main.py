bd06d2ed264fbc65e3d54757b6c731bd17d97781
Sleonow <kazanofffa19846494@gmail.com>
2025-05-21 18:25:58 +0300

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-bot.remove_webhook()
-bot.set_webhook(url="https://olx-telegram-bot.onrender.com/YOUR_TOKEN")
-
-
-if __name__ == '__main__':
-    bot.remove_webhook()
-    bot.set_webhook(url=f"{WEBHOOK_URL}/{TOKEN}")
-    app.run(host="0.0.0.0", port=10000)
